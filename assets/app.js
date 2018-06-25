237963f63819725c62d4472a6bb93d71467f2fa3
Jose De Las Salas <SueBuSo@Joses-MacBook-Pro.local>
2018-06-25 17:16:52 -0400

    added preliminary firebase code to link train system to firebase database.

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBdoJiMy_PFktLmrveH0A2nbbj8wtvur_o",
+    authDomain: "trainsystemhw.firebaseapp.com",
+    databaseURL: "https://trainsystemhw.firebaseio.com",
+    projectId: "trainsystemhw",
+    storageBucket: "trainsystemhw.appspot.com",
+    messagingSenderId: "695028065097"
+  };
+  firebase.initializeApp(config);
+</script>
