4af05ba8197c5b7b680d7c5852cbdf9a687c7de5
Joed Machado <joedmg58@gmail.com>
2018-06-24 22:44:54 -0400

    Function add train created

---
+++ assets/js/app.js
@@ -0,0 +1,74 @@
+/*
+** Train Schedule App
+** Application for Homework Week 7
+** Joseph Ragno, Jose De Las Salas, Raul and Joed Machado. 2018 UM Coding Boot Camp.
+*/
+
+//some global variables
+addButtonId = ''; //The ID name for the button designated to add train schedule
+tableId =''; //The ID of the table where the train schedule is shown
+//formId =''; //The ID of the form used to collect the information about the train schedule
+trainNameInput = ''; //ID of the input text to collect the train name
+trainDestinationInput = ''; // id of the destination input text
+trainFirstTimeInput = ''; // id of the first time input
+trainFrequencyInput = ''; // id of the frequency input
+
+//It will start when the document will finish to load
+$(document).ready( function() {
+
+    // Initialize Firebase
+    var databaseName = ''; //we need to fill this
+
+    var config = {
+        apiKey: "", //we need to fill this
+        authDomain: databaseName + ".firebaseapp.com",
+        databaseURL: "https://" + databaseName + ".firebaseio.com",
+        projectId: databaseName,
+        storageBucket: "",
+        messagingSenderId: "" //we need to fill this 
+    };
+
+    firebase.initializeApp(config);
+
+    //Reference to the train schedule database
+    var fbTrainSchedule = firebase.database();
+    console.log( fbTrainSchedule );
+
+    //Registering onclick event for adding train
+    $( addButtonId ).click( addTrain );
+
+    //Function for adding a train to the database
+    function addTrain( event ) {
+        event.preventDefault();
+    
+        var trainName = $( trainNameInput ).val().trim();
+        var trainDestination = $( trainDestinationInput ).val().trim();
+        var trainFirstTime = $( trainFirstTimeInput ).val().trim();
+        var trainFrequency = $( trainFrequencyInput ).val().trim();
+    
+        fbTrainSchedule.ref().push( {
+            name: trainName,
+            destination: trainDestination,
+            first: trainFirstTime,
+            frequency: trainFrequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        } );
+    
+        clearInputs();
+    }
+
+    //clear all the text inputs of the form
+    function clearInputs() {
+        $( trainNameInput ).val('');
+        $( trainDestinationInput ).val('');
+        $( trainFirstTimeInput ).val('');
+        $( trainFrequencyInput ).val('');
+    }
+
+    //Its triggers when a new child (element) to the database is added
+    fbEmployees.ref().on( "child_added", function( snapshot ) {
+        
+    } );
+
+} );
+ 
